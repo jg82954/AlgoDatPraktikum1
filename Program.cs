3ec3c20833c37838069124d5e05e57cdadbaaf3e
Jan Gößwein <goessweinja82954@TechnischeHochschuleN.onmicrosoft.com>
2022-04-20 17:08:27 +0200

    Test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgoDatPraktikum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
