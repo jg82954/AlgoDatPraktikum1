b9faa30a5982bbbee46f9fe759d33c528ce9cc9b
klimovva82148@TechnischeHochschuleN.onmicrosoft.com <klimovva82148@TechnischeHochschuleN.onmicrosoft.com>
2022-04-20 17:36:07 +0200

    Test sollte klappen

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace AlgoDatPraktikum
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("klappt, vkl");
         }
     }
 }
