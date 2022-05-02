dfb73a6187d99203a5c14d041d6d91d8560864e9
haberstockja83676@TechnischeHochschuleN.onmicrosoft.com <haberstockja83676@TechnischeHochschuleN.onmicrosoft.com>
2022-05-02 17:09:30 +0200

    Erstes mal Arrays Pushen

---
+++ Arrays/Array.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoDatPraktikum
+{
+	abstract class Array : IDictionary
+	{
+		public Array(params int[] elems)
+		{
+			length = elems.Length;
+			foreach (int item in elems)
+			{
+				insert(item); // noch testen welche isert Methode er hier nimmt
+			}
+		}
+
+		const int maxlength = 50;
+		int length;
+		int aktpos;
+		int[] data = new int[maxlength];
+
+		// indexer
+
+		public void print()// ist überall gleich 
+		{
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(data[i] + " ");
+            }
+		}
+		public abstract bool insert(int elem);
+		public abstract bool delete(int elem);
+		public abstract bool search(int elem);
+        //protected int _search_(int elem)
+        //{
+        //    return 3;
+        //}
+    }
+}
