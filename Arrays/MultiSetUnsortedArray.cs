24315fad4c2cc70902460b6e6461ed192975adb3
haberstockja83676@TechnischeHochschuleN.onmicrosoft.com <haberstockja83676@TechnischeHochschuleN.onmicrosoft.com>
2022-05-02 17:12:44 +0200

    Arrays

---
+++ Arrays/MultiSetUnsortedArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoDatPraktikum
+{
+	class MultiSetUnsortedArray: Array, IMultiSetUnsorted
+	{
+		public MultiSetUnsortedArray()
+		{
+
+		}
+
+        public override bool insert(int elem)
+        {
+            bool inserted = false;
+
+            return inserted;
+        }
+        public override bool delete(int elem)
+        {
+            bool deleted = false;
+
+            return deleted;
+        }
+        public override bool search(int elem)
+        {
+            bool found = false;
+
+            return found;
+        }
+    }
+}
